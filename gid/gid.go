d864f763c03d1cc461d9bdb795c4455ed83e5f04
Coloured-glaze <1044187534@qq.com>
2022-12-01 16:14:16 +0800

    update

---
+++ gid/gid.go
@@ -0,0 +1,17 @@
+package gid
+
+import (
+	"bytes"
+	"runtime"
+	"strconv"
+)
+
+// 获取 goroutine id
+func GetGID() int64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseInt(string(b), 10, 64)
+	return n
+}
