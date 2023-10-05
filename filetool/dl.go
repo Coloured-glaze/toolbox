ea56d414a07d1c8c246e2a017073b87aac8d9d4c
Coloured-glaze <1044187534@qq.com>
2023-10-05 17:48:16 +0800

    update fies.

--- filetool/dl.go
+++ filetool/dl.go
@@ -15,43 +15,47 @@ var (
 	nochkcrtcli = &http.Client{Transport: tr}
 )
 
-// DownloadTo 下载到路径
-func DownloadTo(url, file string, chkcrt bool) error {
+// 下载文件
+func DownloadTo(url, file string, chkcrt bool) (n int64, err error) {
 	var resp *http.Response
-	var err error
-	if chkcrt {
+	switch chkcrt {
+	case true:
 		resp, err = http.Get(url)
-	} else {
-		resp, err = nochkcrtcli.Get(url) //
+	case false:
+		resp, err = nochkcrtcli.Get(url)
 	}
 	if err == nil {
 		defer resp.Body.Close()
 		total := int64(0)
-		rsize := resp.Header.Get("Content-Length")
-		size := resp.ContentLength // 响应内容的大小
+		size := resp.ContentLength
+		rsize := resp.Header.Get("Content-Length") // 响应内容的大小
 		if rsize != "" {
 			total, err = strconv.ParseInt(rsize, 10, 64) // 总计
 		}
 		if err == nil {
-			if total > 100*1024*1024 || size > 100*1024*1024 { // 100MiB
-				// fmt.Println("持续写入")
+			switch {
+			case total > 100*1024*1024 || size > 100*1024*1024: // 100MiB
 				var f *os.File
-				f, err = os.Create(file)
+				f, err = os.Create(file) // 持续写入
 				if err == nil {
 					defer f.Close()
 					_, err = io.Copy(f, resp.Body) // 使用 io.Copy 函数将响应体复制到文件中
 				}
-			} else {
-				// fmt.Println("一次性写入", total, size)
+			default:
 				var data []byte
 				data, err = io.ReadAll(resp.Body)
 				if err == nil {
-					err = os.WriteFile(file, data, 0700)
+					err = os.WriteFile(file, data, 0700) // 一次性写入文件
 				}
 			}
 		}
+		if size > 0 {
+			n = size
+		} else if total > 0 {
+			n = total
+		}
 	}
-	return err
+	return
 }
 
 // IsExist 文件/路径存在
