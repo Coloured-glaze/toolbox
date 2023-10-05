0542d39440bf3398116b4428e016ffb6808c2f1f
Coloured-glaze <1044187534@qq.com>
2023-10-05 16:14:32 +0800

    update files.

--- filetool/dl.go
+++ filetool/dl.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 var (
@@ -21,16 +22,34 @@ func DownloadTo(url, file string, chkcrt bool) error {
 	if chkcrt {
 		resp, err = http.Get(url)
 	} else {
-		resp, err = nochkcrtcli.Get(url)
+		resp, err = nochkcrtcli.Get(url) //
 	}
 	if err == nil {
-		var f *os.File
-		f, err = os.Create(file)
-		if err == nil {
-			_, err = io.Copy(f, resp.Body)
-			f.Close()
+		defer resp.Body.Close()
+		total := int64(0)
+		rsize := resp.Header.Get("Content-Length")
+		size := resp.ContentLength // 响应内容的大小
+		if rsize != "" {
+			total, err = strconv.ParseInt(rsize, 10, 64) // 总计
 		}
-		resp.Body.Close()
+		if err == nil {
+			if total > 100*1024*1024 || size > 100*1024*1024 { // 100MiB
+				// fmt.Println("持续写入")
+				var f *os.File
+				f, err = os.Create(file)
+				if err == nil {
+					defer f.Close()
+					_, err = io.Copy(f, resp.Body) // 使用 io.Copy 函数将响应体复制到文件中
+				}
+			} else {
+				// fmt.Println("一次性写入", total, size)
+				var data []byte
+				data, err = io.ReadAll(resp.Body)
+				if err == nil {
+					err = os.WriteFile(file, data, 0700)
+				}
+			}
+		}
 	}
 	return err
 }
